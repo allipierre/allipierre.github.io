08c1f6be58fb1954f8e8a5428fe00beaceb32d3c
allipierre <yottiallipierre40@yahoo.fr>
2017-08-25 15:06:26 +0200

    Add Bar and Area Chart

--- js/viewModels/incidents.js
+++ js/viewModels/incidents.js
@@ -5,7 +5,7 @@
 /*
  * Your incidents ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'promise', 'ojs/ojmodel', 'ojs/ojtable', 'ojs/ojcollectiontabledatasource'],
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'promise', 'ojs/ojmodel', 'ojs/ojtable', 'ojs/ojcollectiontabledatasource','ojs/ojbutton', 'ojs/ojchart', 'ojs/ojtoolbar'],
  function(oj, ko, $) {
   
     function IncidentsViewModel() {
@@ -41,6 +41,69 @@ self.DeptCol(new self.DeptCollection());
                          
                         self.datasource(new oj.CollectionTableDataSource(self.DeptCol()));
                       }); 
+                      
+                      
+                      
+                      
+                      
+         /* toggle button variables */
+        self.stackValue = ko.observable('off');
+        self.orientationValue = ko.observable('vertical');
+        
+        /* chart data */
+        var areaSeries = [{name : "Series 1", items : [74, 42, 70, 46]},
+                          {name : "Series 2", items : [50, 58, 46, 54]},
+                          {name : "Series 3", items : [34, 22, 30, 32]},
+                          {name : "Series 4", items : [18,  6, 14, 22]}];
+    
+        var areaGroups = ["Group A", "Group B", "Group C", "Group D"];
+   
+        
+        this.areaSeriesValue = ko.observableArray(areaSeries);
+        this.areaGroupsValue = ko.observableArray(areaGroups);
+        
+        /* toggle buttons*/
+        self.stackOptions = [
+            {id: 'unstacked', label: 'unstacked', value: 'off', icon: 'oj-icon demo-area-vert'},
+            {id: 'stacked', label: 'stacked', value: 'on', icon: 'oj-icon demo-area-stack'}
+        ];
+        self.orientationOptions = [
+            {id: 'vertical', label: 'vertical', value: 'vertical', icon: 'oj-icon demo-area-vert'},
+            {id: 'horizontal', label: 'horizontal', value: 'horizontal', icon: 'oj-icon demo-area-horiz'}
+        ]; 
+        
+        
+        
+        
+        
+        
+        
+        
+        /* toggle button variables */
+        self.stackValue = ko.observable('off');
+        self.orientationValue = ko.observable('vertical');
+        
+        /* chart data */
+        var barSeries = [{name: "Series 1", items: [42, 34]},
+                         {name: "Series 2", items: [55, 30]},
+                         {name: "Series 3", items: [36, 50]},
+                         {name: "Series 4", items: [22, 46]},
+                         {name: "Series 5", items: [22, 46]}];
+    
+        var barGroups = ["Group A", "Group B"];
+   
+        self.barSeriesValue = ko.observableArray(barSeries);
+        self.barGroupsValue = ko.observableArray(barGroups);
+        
+        /* toggle buttons*/
+        self.stackOptions = [
+            {id: 'unstacked', label: 'unstacked', value: 'off', icon: 'oj-icon demo-bar-unstack'},
+            {id: 'stacked', label: 'stacked', value: 'on', icon: 'oj-icon demo-bar-stack'}
+        ];
+        self.orientationOptions = [
+            {id: 'vertical', label: 'vertical', value: 'vertical', icon: 'oj-icon demo-bar-vert'},
+            {id: 'horizontal', label: 'horizontal', value: 'horizontal', icon: 'oj-icon demo-bar-horiz'}
+        ];
       // Below are a subset of the ViewModel methods invoked by the ojModule binding
       // Please reference the ojModule jsDoc for additionaly available methods.
 
